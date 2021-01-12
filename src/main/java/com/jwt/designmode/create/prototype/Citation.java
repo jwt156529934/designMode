1c0c213e078ffb734a762e3d408e8be7d1d66569
jwt <156529934@qq.com>
2021-01-12 19:56:46 +0800

    原型模式

---
+++ src/main/java/com/jwt/designmode/create/prototype/Citation.java
@@ -0,0 +1,26 @@
+package com.jwt.designmode.create.prototype;
+
+/**
+ * @author jwt
+ * @date 2021/1/12 7:37 下午
+ */
+public class Citation implements Cloneable{
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return (this.name);
+    }
+
+    public void show() {
+        System.out.println(name + "同学：在2020学年第一学期中表现优秀，被评为三好学生。特发此状！");
+    }
+
+    @Override
+    public Citation clone() throws CloneNotSupportedException {
+        return (Citation) super.clone();
+    }
+}
